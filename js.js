523ab92114eb79468ae61927b8d91b2904a18b7f
sasal22 <sasal.kross@gmail.com>
2024-01-24 16:49:01 +0600

    init

---
+++ js.js
@@ -0,0 +1,10 @@
+let formbox = document.querySelector(".form")
+let burger = document.querySelector(".burger")
+let burgerBtn = document.querySelector(".buger__btn")
+let popup = ()=>{
+formbox.classList.toggle("active")
+}
+let toggleBurger = ()=> {
+    burger.classList.toggle("active")
+    burger_Btn.classList.toggle("active")
+}
